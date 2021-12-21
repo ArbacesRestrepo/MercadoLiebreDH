e2feb8c97b7447764e7e2d2d39a08fd95f64fe6a
ArbacesRestrepo <arbacesrestrepo1972@gmail.com>
2021-12-20 21:29:37 -0500

    Pera publicar

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ app.get('', (req,res) =>{
 app.use(express.static( path.join( __dirname, './public') ) );  
 app.use(express.static( path.join( __dirname, './views') ) );  
 
-app.listen(3000, () => {
+app.listen( process.env.PORT || 3000, () => {
     console.log("Servidor corriendo");
 });
 

13ea1958acc8481b5732728f51bd9cbdabbf2806
ArbacesRestrepo <arbacesrestrepo1972@gmail.com>
2021-12-20 21:04:13 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('', (req,res) =>{
+    //res.send("Ahora si entre");  // Permite enviar texto o codigo HTML
+    res.sendFile( ( __dirname + '/Views/Home.html' ) );  // Permite enviar archivo HTML
+});
+
+app.use(express.static( path.join( __dirname, './public') ) );  
+app.use(express.static( path.join( __dirname, './views') ) );  
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo");
+});
+
+
+
